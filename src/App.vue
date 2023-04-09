fbf54dd93e64a04312b3f734768bdbdac165746d
Lovell Liu <lovelliu@foxmail.com>
2023-04-09 20:28:42 +0800

    feat: add HelloWorld comp

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-const count = ref(0)
 </script>
 
 <template>
-  <div class="text-red-500">
-    {{ count }}
-  </div>
+  <RouterView />
 </template>
 
 <style scoped>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { useCounterStore } from '@/store/modules/counter'
+
+const counter = useCounterStore()
+
+counter.increment()
+</script>
+
+<template>
+  <div class="text-red-500 mr-1 p-4">
+    Current Count: {{ counter.count }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
