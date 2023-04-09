cc5cf1291c1916a779ca0b7fc4bed4694d6463cb
Lovell Liu <lovelliu@foxmail.com>
2023-04-09 20:27:24 +0800

    feat: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const pathResolve = (dir: string) => resolve(process.cwd(), '.', dir)
+
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: pathResolve('src') },
+      { find: '#', replacement: pathResolve('types') },
+    ],
+  },
 })
